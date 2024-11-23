69da78a53d4e1888420fdc8ed234cfa52f437627
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-11-23 17:35:33 +0100

    Combined Nav an Logo

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,6 +1,4 @@
-import Header from "./Header/Header.jsx";
 import NavBar from "./NavBar/NavBar.jsx";
-import Logo from "./NavBar/Logo.jsx";
 import ChooseUs from "./ChooseUs/ChooseUs.jsx";
 import AppOffers from "./AppOffers/AppOffers.jsx";
 import MembershipPlan from "./MembershipPlan/MembershipPlan.jsx";
@@ -9,19 +7,19 @@ import styles from "./LandingPage.module.css";
 function LandingPage() {
   return (
     <>
-      <div className={styles.first_div}>
-        <NavBar />
-        <Logo />
-        <Header />
-      </div>
-      <div className={styles.AppOffers}>
-        <AppOffers />
-      </div>
-      <div className={styles.ChooseUs}>
-        <ChooseUs />
-      </div>
-      <div className={styles.MembershipPlan}>
-        <MembershipPlan />
+      <div>
+        <div className={styles.first_div}>
+          <NavBar />
+        </div>
+        <div className={styles.AppOffers}>
+          <AppOffers />
+        </div>
+        <div className={styles.ChooseUs}>
+          <ChooseUs />
+        </div>
+        <div className={styles.MembershipPlan}>
+          <MembershipPlan />
+        </div>
       </div>
     </>
   );
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,17 +1,27 @@
 import styles from "./NavBar.module.css";
+import Logo from "./Logo.jsx";
+import Header from "../Header/Header.jsx";
 
 function NavBar() {
   return (
-    <nav>
-      <div className={styles.navbar}>
-        <div className={styles.middleSection}>
-          <div>About </div>
-          <div>Pricing </div>
-          <div>Contact us </div>
+    <>
+      <div className={styles.navAndHeader}>
+        <div className={styles.navAndLogo}>
+          <Logo />
+          <nav>
+            <div className={styles.navbar}>
+              <div className={styles.middleSection}>
+                <div>About </div>
+                <div>Pricing </div>
+                <div>Contact us </div>
+              </div>
+              <div className={styles.join}>JOIN NOW</div>
+            </div>
+          </nav>
         </div>
-        <div className={styles.join}>JOIN NOW</div>
+        <Header />
       </div>
-    </nav>
+    </>
   );
 }
 
