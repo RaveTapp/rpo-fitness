665c6d5364a2f5f67ae4c03e9d85c74d3ed0227f
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-11-23 08:30:22 +0100

    Add Exercise Item
    Increase hover effect on plus button

--- src/components/WorkoutList/WorkoutList.jsx
+++ src/components/WorkoutList/WorkoutList.jsx
@@ -1,9 +1,10 @@
 import ListCSS from "./WorkoutList.module.css"
 import { PlusBtn } from "../plusBtn/plusBtn";
+import { ExerciseItem } from "./ExerciseItem";
 
 export function WorkoutList() {
 
-    
+
     return (
         <div className={ListCSS.menu}>
             <div className={ListCSS.mainTitle}>
@@ -13,13 +14,13 @@ export function WorkoutList() {
             <hr className={ListCSS.hr} />
             <ul>
                 <li className={ListCSS.item}>
-                    
+                    <ExerciseItem title="Barbell Lunge" />
                 </li>
                 <li className={ListCSS.item}>
-
+                    <ExerciseItem title="Front Squat" />
                 </li>
                 <li className={ListCSS.item}>
-                    
+                    <ExerciseItem title="Leg Curls" />
                 </li>
             </ul>
             <PlusBtn />
