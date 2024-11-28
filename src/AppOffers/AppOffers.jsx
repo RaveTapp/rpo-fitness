a05e4d474e94c9e2473af9eeb5bcb845f9bcf7f3
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-11-28 22:43:31 +0100

    change firstblock of appoffers, change header

--- src/AppOffers/AppOffers.jsx
+++ src/AppOffers/AppOffers.jsx
@@ -1,23 +1,39 @@
 import styles from "./AppOffers.module.css";
+
 function AppOffers() {
   return (
     <>
       <div className={styles.AppOffers}>
         <div className={styles.FirstBlock}>
-          <h1>PROGRESS TRACKING</h1>
-          <img></img>
+          <div className={styles.LeftSection}>
+            <h1>PROGRESS TRACKING</h1>
+            <div className={styles.Text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
+            </div>
+          </div>
+          <img
+            className={styles.Image}
+            src="https://via.placeholder.com/150"
+            alt="progress tracking"
+          ></img>
         </div>
+
         <div className={styles.SecondBlock}>
+          <img src="https://via.placeholder.com/150"></img>
           <h1>HEALTHY RECIPES</h1>
-          <img></img>
         </div>
         <div className={styles.ThirdBlock}>
           <h1>PERSONAL PROGRAM</h1>
-          <img></img>
+          <img src="https://via.placeholder.com/150"></img>
         </div>
         <div className={styles.FourthBlock}>
           <h1>COMPETING WITH FRIENDS</h1>
-          <img></img>
+          <img src="https://via.placeholder.com/150"></img>
         </div>
       </div>
     </>
