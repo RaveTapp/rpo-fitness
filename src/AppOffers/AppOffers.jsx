05e3a1d57e06895c306672f29f01aac4a90ae22d
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-11-28 23:54:43 +0100

    Change fourth block of appoffers

--- src/AppOffers/AppOffers.jsx
+++ src/AppOffers/AppOffers.jsx
@@ -50,8 +50,18 @@ function AppOffers() {
           <img src="https://via.placeholder.com/150"></img>
         </div>
         <div className={styles.FourthBlock}>
-          <h1>COMPETING WITH FRIENDS</h1>
           <img src="https://via.placeholder.com/150"></img>
+          <div className={styles.RightSection}>
+            <h1>COMPETING WITH FRIENDS</h1>
+            <div className={styles.Text}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur.
+            </div>
+          </div>
         </div>
       </div>
     </>
