d603aaf39729a557206daf595afebcee24910321
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-11-30 11:17:35 +0100

    Change UpgreadeToPremium

--- src/MembershipPlan/MembershipPlan.jsx
+++ src/MembershipPlan/MembershipPlan.jsx
@@ -14,10 +14,28 @@ function MembershipPlan() {
 
         <div className={styles.cards}>
           <div className={styles.firstCard}>
-            <h1>Free Trail</h1>
+            <p className={styles.headerText}>Free Trail</p>
+            <p>30 days only</p>
+            <hr className={styles.line}></hr>
+            <div className={styles.freeComponents}>
+              <p>Limited Generation</p>
+              <p>Exercise Library</p>
+              <p>Join Challenges</p>
+            </div>
+            <div className={styles.buttonTryNow}>Try Now</div>
           </div>
           <div className={styles.secondCard}>
-            <h1>Premium</h1>
+            <p className={styles.headerText2}>Premium</p>
+            <span>X$ per month</span>
+            <span>X$ per year</span>
+            <hr className={styles.line2}></hr>
+            <div className={styles.premiumComponents}>
+              <p>Unlimited Workout Generation</p>
+              <p>Workout Progression</p>
+              <p>Challenge Creation</p>
+              <p>Easy Nutrition Tracking</p>
+            </div>
+            <div className={styles.buttonJoin}>Join</div>
           </div>
         </div>
       </div>
