30d5260c4a6a41a41fc004ac554a4b8b07f08c91
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-12-02 23:03:57 +0100

    change look

--- src/signup/signup.jsx
+++ src/signup/signup.jsx
@@ -7,26 +7,26 @@ function SignUp() {
         <div className={styles.Member}>Already A Member? Log in</div>
         <div className={styles.Name}>
           <div className={styles.firstName}>
-            <label>First name</label>
-            <input type="text"></input>
+            <label>First Name</label>
+            <input type="text" placeholder="First Name"></input>
           </div>
           <div className={styles.lastName}>
-            <label>Last name</label>
-            <input type="text"></input>
+            <label>Last Name</label>
+            <input type="text" placeholder="Last Name"></input>
           </div>
         </div>
         <div className={styles.Email}>
           <label>Email</label>
-          <input type="email"></input>
+          <input type="email" placeholder="Email"></input>
         </div>
         <div className={styles.Password}>
           <div className={styles.PasswordInput}>
             <label>Password</label>
-            <input type="password"></input>
+            <input type="password" placeholder="Password"></input>
           </div>
           <div className={styles.ConfirmPassword}>
             <label>Confirm Password</label>
-            <input type="password"></input>
+            <input type="password" placeholder="Confirm Password"></input>
           </div>
         </div>
         <div className={styles.CreateButton}>Create account</div>
