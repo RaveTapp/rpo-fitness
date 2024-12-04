8f1990865ed26c3b8c92631bc767f4c3a052821f
nik003k <nik.ptuj@gmail.com>
2024-12-04 17:02:08 +0100

    brisanje Buttona

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
-import Button from "./Button/Button.jsx";
 import Card from "./Card/Card.jsx";
 import SectionName from "./SectionName/SectionName.jsx";
 import CalcBMI from "./CalcBMI/CalcBMI.jsx";
 import exercisePic from "./assets/exercise.webp";
 import nutritionPic from "./assets/hrana1.jpg";
-import graf from "./assets/graf.png"
 
 function App() {
 
