584743bc2db55f47a84b42d62b8c880c89f500dd
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-12-10 23:58:45 +0100

    last version?

--- src/NavBar/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./NavBar.module.css";
-import logo from "../assets/logo.png";
-
-function Logo() {
-  return <img src={logo} alt="site-logo" className={styles.logo}></img>;
-}
-
-export default Logo;
