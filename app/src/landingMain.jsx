ad79032d7a84a13c30c546e1be13fa7addebe667
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-12-11 18:18:21 +0100

    Route Landing Page

--- app/src/landingMain.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LandingPage from "./LandingPage";
-import ReactDOM from "react-dom/client";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>
-);
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReneMain from "./reneMain";
+import LandingPage from "./LandingPage";
 
 export default function App(){
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />}/>
         <Route path="/workout/*" element={<ReneMain />}/>
       </Routes>
     </BrowserRouter>
