8195171496d3b52dfa34ee385943406b2d6e53c0
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-12-15 15:50:03 +0100

    edit forgotPassword form

--- app/src/forgotPassword/forgotPassword.jsx
+++ app/src/forgotPassword/forgotPassword.jsx
@@ -1,26 +1,36 @@
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 import styles from "./forgotPassword.module.css";
 
-function forgotPassword() {
-  const navigate = useNavigate();
+function ForgotPassword() {
+  const [emailSent, setEmailSent] = useState(false);
 
-  const handleSubmit = (e) => {};
+  const handleSendCode = () => {
+    setEmailSent(true);
+  };
 
   return (
     <>
       <div className={styles.forgotPasswordDiv}>
         <div className={styles.forgotPasswordBox}>
-          <h1 className={styles.headerText}>Enter your code here:</h1>
-          <div className={styles.input}>
-            <input type="text" placeholder="Enter your code here"></input>
-          </div>
-          <div className={styles.buttonSubmit} onClick={handleSubmit}>
-            Submit
-          </div>
+          <h1 className={styles.headerText}>Forgot Password</h1>
+          {!emailSent ? (
+            <div className={styles.buttonSendCode} onClick={handleSendCode}>
+              Send Verification Code
+            </div>
+          ) : (
+            <div>
+              <div className={styles.input}>
+                <input type="text" placeholder="Enter your code here" />
+              </div>
+              <div type="submit" className={styles.buttonSubmit}>
+                Submit
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
   );
 }
 
-export default forgotPassword;
+export default ForgotPassword;
