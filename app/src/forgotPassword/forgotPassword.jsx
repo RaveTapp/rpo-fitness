7227d7a7b5daf8bd8f8ba25ce7e420eff6d21c82
NikMori <148778780+Denomi12@users.noreply.github.com>
2024-12-15 18:17:17 +0100

    not finished code verification

--- app/src/forgotPassword/forgotPassword.jsx
+++ app/src/forgotPassword/forgotPassword.jsx
@@ -1,11 +1,39 @@
 import React, { useState } from "react";
 import styles from "./forgotPassword.module.css";
+import emailjs from "emailjs-com";
 
 function ForgotPassword() {
+  const [userEmail, setUserEmail] = useState("");
   const [emailSent, setEmailSent] = useState(false);
+  const [verificationCode, setVerificationCode] = useState("");
 
-  const handleSendCode = () => {
-    setEmailSent(true);
+  const generateCode = () => {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+  };
+
+  // Funkcija za pošiljanje e-pošte
+  const handleSendCode = async (e) => {
+    e.preventDefault();
+
+    const code = generateCode();
+    setVerificationCode(code);
+
+    try {
+      const result = await emailjs.send(
+        "service_bxiwqka", // ID tvoje storitve (EmailJS)
+        "template_nnnypwt", // ID predloge
+        {
+          to_name: "User",
+          to_email: "nmori986@gmail.com",
+          verification_code: code,
+        },
+        "PlvE2HRTmAhWBdMul" // javni ključ iz EmailJS
+      );
+      console.log("Email sent successfully:", result.text);
+      setEmailSent(true);
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
   };
 
   return (
