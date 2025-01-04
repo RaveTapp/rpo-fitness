ea4c7238e103a87823c128c5ad39c73579ab1829
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2025-01-04 07:38:58 +0100

    Fetch exercise from API and display description

--- app/src/components/ExerciseShowcase/ExerciseShowcase.jsx
+++ app/src/components/ExerciseShowcase/ExerciseShowcase.jsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router"
 import ExerciseCSS from "./ExerciseShowcase.module.css"
 import CSS from "../../main.module.css"
 
+const wholeData = await fetch("https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json");
+const wholeJSON = await wholeData.json();
+
 async function getTableOffset(table, n, setData){
     try {
         const response = await fetch(`http://localhost:3000/exercisesOffset`, {
@@ -34,21 +37,47 @@ export function ExerciseShowcase(props) {
     let {exerciseId} = useParams();
 
     const [data, setData] = useState([]);
+    const [exercise, setExercise] = useState([]);
 
     useEffect(() => {
             getTableOffset("vaja", exerciseId, setData);
     }, []);
 
+    useEffect(() => {
+        if(Object.keys(data).length > 0){
+            getExerciseJSON(data.rows[0].ime, setExercise);
+        }
+    }, [data]);
+
+    let description = [];
+    if(Object.keys(exercise).length > 0){
+        description.splice(0, description.length);
+
+        exercise.instructions.forEach((e, i) => {
+            description.push(<p className={`${CSS.tekst} ${ExerciseCSS.tekst}`} key={"exerciseDesc" + i}>{e}</p>);
+        });
+    }
+
     return (
         <div className={ExerciseCSS.menu}>
             <div className={ExerciseCSS.mainTitle}>
-                <h1 className={`${CSS.tekst}`}>{data.length == 1 ? data.rows[0].ime : "Title not found"}</h1>
+                <h1 className={`${CSS.tekst}`}>{Object.keys(data).length > 0 ? data.rows[0].ime : "Title not found"}</h1>
             </div>
             <div className={ExerciseCSS.imageContainer}>
                 <img src="" alt="" />
                 <img src="" alt="" />
             </div>
-            <p className={`${CSS.tekst} ${ExerciseCSS.tekst}`}>{props.exerText}</p>
+            {description}
         </div>
     );
+}
+
+async function getExerciseJSON(title, setExercise){
+    if(Object.keys(wholeJSON).length > 0){
+        wholeJSON.forEach(e => {
+            if(e.name == title){
+                return setExercise(e);
+            }
+        });
+    }
 }
