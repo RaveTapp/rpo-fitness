86bf80256afca8190096a589499655d40bb4993a
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2025-01-08 06:34:43 +0100

    Remove unnecessary routes

--- app/src/signupMain.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SignUp from "./signup/signup.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SignUp />
-  </React.StrictMode>
-);
